909129c00a7f38534b7a53392e6ee4c94283cc91
rohith420 <139437364+rohithsigma6@users.noreply.github.com>
2023-09-12 14:31:23 +0530

    done
    done

--- election/src/utils/config.js
+++ election/src/utils/config.js
@@ -1,2 +1,4 @@
-const API_DEV_URL = `http://localhost:5678/v1`
+// const API_DEV_URL = `http://localhost:5678/v1`
+const API_DEV_URL = `https://election-backend-1.onrender.com/v1`
+
 export default API_DEV_URL
